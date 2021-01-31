38dccc6a4d09ecdae0b826907d53d533628f105a
Noah Beaudin <noah.beaudin1@gmail.com>
2021-01-31 07:40:13 -0700

    Added basic code for titleScreen ➕

---
+++ src/ts/titleScreen.ts
@@ -0,0 +1,13 @@
+// Script for titleScreen.html
+
+const playButton = document.getElementById("playButton")
+const usernameInput = document.getElementById("usrnameInput")
+
+let username:string
+let comPlayerAmount:number
+
+// onClick function for playButton
+
+function startGame() {
+
+}

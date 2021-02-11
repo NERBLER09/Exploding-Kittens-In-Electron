07ebbc0eff39e36866b783f5a3c2c591231bf3ef
Noah Beaudin <noah.beaudin1@gmail.com>
2021-02-11 15:40:11 -0700

    Updated global and file css ➕

--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ let mainWindow, gameWindow
 const createWindow = () => {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1000,
+    height: 800,
     webPreferences: {
       nodeIntegration: true
     }

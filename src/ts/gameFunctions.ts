fbc60b80a861816b871ef2733aefa81752d41c93
Noah Beaudin <noah.beaudin1@gmail.com>
2021-02-14 15:22:36 -0700

    Added baic card functionality ➕

--- src/ts/gameFunctions.ts
+++ src/ts/gameFunctions.ts
@@ -10,24 +10,33 @@ const $ = require("jquery")
 // Contains how many total cards are in the deck
 let totalCardAmount = 51
 
+// Stores if it is the player's turn
+let isPlayerTurn = true
+
 // Draws a card for the player
 const drawCard = () => {
-    // Checks if there are still cards in the deck
-    if(totalCardAmount != 0) {
-        // Choses a card
-        const cardIndex = Math.floor(Math.random() * cards.length)
-        const card = cards[cardIndex];
+    // Checks if its the players turn
+    if(isPlayerTurn === true) {
+        // Checks if there are still cards in the deck
+        if(totalCardAmount != 0) {
+            // Choses a card
+            const cardIndex = Math.floor(Math.random() * cards.length)
+            const card = cards[cardIndex];
 
-        // Adds the drawn card the the list
-        playerCardsInHand.push(card)
+            // Adds the drawn card the the list
+            playerCardsInHand.push(card)
 
-        // console.log(`Card drawn is: ${card}. Index ${cardIndex}`)
+            // console.log(`Card drawn is: ${card}. Index ${cardIndex}`)
 
-        // Removes the drawn card from the deck
-        removeDrawnCardFromDeck(card)
+            // Removes the drawn card from the deck
+            removeDrawnCardFromDeck(card)
 
-        // Displays the drawn card
-        displayDrawnCard(card)
+            // Displays the drawn card
+            displayDrawnCard(card)
+
+            // Sets isPlayerTurn to false (Uncomment when done working on card functionality)
+            // isPlayerTurn = false 
+        }
     }
 }
 
@@ -45,25 +54,31 @@ const displayDrawnCard = (cardToDisplay:string) => {
 
 // Function for when the player plays a card
 const playCard = (playerCard) => {
-    const cardPlayed = playerCard.data.param1
+    // Checks if its the player's turn
+    if(isPlayerTurn === true) {
+        const cardPlayed = playerCard.data.param1
 
-    // Loops through the cards in the player hand
-    for(const checkForCardInHand of playerCardsInHand) {
-        // Checks if the looped card matches the played card
-        if(checkForCardInHand === cardPlayed) {
-            let cardIndex = playerCardsInHand.indexOf(cardPlayed)
+        // Loops through the cards in the player hand
+        for(const checkForCardInHand of playerCardsInHand) {
+            // Checks if the looped card matches the played card
+            if(checkForCardInHand === cardPlayed) {
+                let cardIndex = playerCardsInHand.indexOf(cardPlayed)
 
-            // Removes the played card from the playerCardsInHand list
-            playerCardsInHand.splice(cardIndex, 1)
+                // Removes the played card from the playerCardsInHand list
+                playerCardsInHand.splice(cardIndex, 1)
 
-            // Hides the played card
-            $(".player_cards").get(cardIndex).remove()
+                // Hides the played card
+                $(".player_cards").get(cardIndex).remove()
 
-            // Breaks the loop
-            break
-        } 
+                // Checks what card is played
+                checkPlayerCardPlayed(checkForCardInHand)
 
-        // console.log(checkForCardInHand)
+                // Breaks the loop
+                break
+            } 
+
+            // console.log(checkForCardInHand)
+        }   
     }   
 }
 
@@ -87,6 +102,41 @@ const removeDrawnCardFromDeck = (cardToRemoveFromDeck: string) => {
     else {
         // Displays remaining the cards in the deck
         $("#remaining_card").html(`Remaining Cards: ${totalCardAmount}`)
+    }
+}
+
+// Checks what card the player played so it can do its respective action
+const checkPlayerCardPlayed = (cardPLayed:string) => {
+    // TODO: Update card functionality to not only print the played card
+
+    // Checks if the player played a cat card (can't be in the switch statement because of the multiple cards)
+    if(cardPLayed == 'potato cat' || cardPLayed == 'taco cat' || cardPLayed == 'rainbow ralphing cat' || 
+    cardPLayed == 'beard cat' || cardPLayed == 'cattermellon') {
+        console.log(`Player played a cat card (${cardPLayed})`)
+    }
+
+    switch(cardPLayed) {
+        case "skip":
+            console.log("Player played a skip")
+            break
+        case "attack":
+            console.log("Player played a attack")
+            break
+        case "shuffle":
+            console.log("Player played a shuffle") 
+            break
+        case "see the future":
+            console.log("Player played a see the future")
+            break
+        case "favor":
+            console.log("Player played a favor")
+            break
+        case "shuffle":
+            console.log("Player played a shuffle")
+            break
+        case "favor":
+            console.log("Player played a favor")
+            break
     }
 }
 
